34a08799c9326a82ae281e0624277f7b9ae1abe4
bluesbreak <ivanivanovat@gmail.com>
2022-06-03 00:00:21 +0300

    first commit
    first commit to github folder

---
+++ Fundamentals C#/Data Types And Variables/05. Special Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = i;
+                double sumOfDigits = 0;
+                while (currentNum > 0)
+                {
+                    sumOfDigits += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+                
+
+
+
+                if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+
+
+                
+            }
+        }
+    }
+}

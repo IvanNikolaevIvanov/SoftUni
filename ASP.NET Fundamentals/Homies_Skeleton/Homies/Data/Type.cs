3a4e5616ad5238698900c6d66b5e2cbc0389fbc6
bluesbreak <ivanivanovat@gmail.com>
2024-02-14 14:02:32 +0200

    C# Web

---
+++ ASP.NET Fundamentals/Homies_Skeleton/Homies/Data/Type.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homies.Data
+{
+    public class Type
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(DataConstants.TypeNameMaxLength)]
+        public string Name { get; set; }
+
+        public IList<Event> Events { get; set; } = new List<Event>();
+
+    }
+}
+

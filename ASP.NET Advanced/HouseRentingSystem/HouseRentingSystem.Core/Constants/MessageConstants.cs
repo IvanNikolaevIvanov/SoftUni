f78d4d1afa249af64405cce8f2739bf4a45bf4de
bluesbreak <ivanivanovat@gmail.com>
2024-03-04 18:04:59 +0200

    Added Core MessageConstants

--- ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Core/Constants/MessageConstants.cs
+++ ASP.NET Advanced/HouseRentingSystem/HouseRentingSystem.Core/Constants/MessageConstants.cs
@@ -4,6 +4,11 @@
     {
         public const string RequiredMessage = "The {0} field is required";
 
-        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long" 
+        public const string LengthMessage = "The field {0} must be between {2} and {1} characters long";
+
+        public const string PhoneExists = "Phone number exists. Enter a different one";
+
+        public const string HasRents = "You should have no rents to become an agent";
     }
 }
+

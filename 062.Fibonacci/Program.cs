f9ee518d237d4d8b8314e674033caf38e9d6a77d
bluesbreak <ivanivanovat@gmail.com>
2024-07-02 19:13:25 +0300

    Recursion Examples Added

---
+++ 062.Fibonacci/Program.cs
@@ -0,0 +1,18 @@
+﻿// 0, 1, 1, 2, 3, 5, 8, 13
+
+int fibonacci = Fibonacci(7);
+Console.WriteLine(fibonacci);
+
+static int Fibonacci(int position)
+{
+	if (position == 0)
+	{
+		return 0;
+	}
+	if (position == 1)
+	{
+		return 1;
+	}
+
+	return Fibonacci(position - 1) + Fibonacci(position - 2);
+}
---
+++ Recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Print(10);
+
+
+static void Print(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    Console.WriteLine($"{n} Before recursion");
+
+    Print(n - 1);
+
+    Console.WriteLine($"{n} After recursion");
+}

859df90341676e3a4d80efc5f0c51f0faf493a05
bluesbreak <ivanivanovat@gmail.com>
2024-07-03 14:38:52 +0300

    SortingAlgorithms Added

---
+++ 07.SortingAlgorithms/MergeSorter.cs
@@ -0,0 +1,70 @@
+﻿namespace _07.SortingAlgorithms
+{
+    public class MergeSorter
+    {
+        public static int[] MergeSort(int[] array)
+        {
+            if (array.Length == 1)
+            {
+                return array;
+            }
+
+            int middle = array.Length / 2;
+            int[] leftArray = PopulateArray(array, 0, middle);
+            int[] rightArray = PopulateArray(array, middle, array.Length);
+
+            Console.WriteLine
+                ($"Left: {string.Join(", ", leftArray)} --- Right: {string.Join(", ", rightArray)}");
+
+            return MergeArrays(MergeSort(leftArray), MergeSort(rightArray));
+        }
+
+        static int[] MergeArrays(int[] left, int[] right)
+        {
+            int index = 0;
+            int[] merged = new int[left.Length + right.Length];
+
+            int leftIndex = 0;
+            int rightIndex = 0;
+
+            while (true)
+            {
+                if (leftIndex < left.Length && rightIndex < right.Length)
+                {
+                    if (left[leftIndex] < right[rightIndex])
+                    {
+                        merged[index++] = left[leftIndex++];
+                    }
+                    else
+                    {
+                        merged[index++] = right[rightIndex++];
+                    }
+                }
+                else if (leftIndex < left.Length)
+                {
+                    merged[index++] = left[leftIndex++];
+                }
+                else if (rightIndex < right.Length)
+                {
+                    merged[index++] = right[rightIndex++];
+                }
+                else
+                {
+                    return merged;
+                }
+            }
+        }
+
+        public static int[] PopulateArray(int[] array, int start, int end)
+        {
+            int[] newArray = new int[end - start];
+
+            for (int i = 0; i <= newArray.Length - 1; i++)
+            {
+                newArray[i] = array[start + i];
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ 07.SortingAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using _07.SortingAlgorithms;
+
+var array = new int[] { 1, 55, 33, 44, 2, 11, 3 };
+
+Console.WriteLine($"Array: {string.Join(", ", array)}");
+
+Console.WriteLine($"Sorted Array: {string.Join(", ", MergeSorter.MergeSort(array))}");
+
+
+
+
+
+
+

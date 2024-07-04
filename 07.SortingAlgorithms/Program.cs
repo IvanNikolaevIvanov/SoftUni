a79048b11a079a42952add4dd7b65ba574de9e09
bluesbreak <ivanivanovat@gmail.com>
2024-07-04 20:19:13 +0300

    Sorting Algorithms Measuring added

--- 07.SortingAlgorithms/Program.cs
+++ 07.SortingAlgorithms/Program.cs
@@ -1,14 +1,30 @@
 ﻿using _07.SortingAlgorithms;
+using System.Diagnostics;
 
-var array = new int[] { 1, 55, 33, 44, 2, 11, 3 };
+//var array = new int[] { 1, 55, 33, 44, 2, 11, 3 };
 
-Console.WriteLine($"Array: {string.Join(", ", array)}");
+//Console.WriteLine($"Array: {string.Join(", ", array)}");
 
-Console.WriteLine($"Sorted Array: {string.Join(", ", MergeSorter.MergeSort(array))}");
+//Console.WriteLine($"Sorted Array: {string.Join(", ", MergeSorter.MergeSort(array))}");
 
+int n = 100_000;
+Random random = new Random();
+int[] arr = new int[n];
+for (int i = 0; i < n; i++)
+{
+    arr[i] = random.Next();
+}
 
+Measure(MergeSorter.MergeSort, arr, "Merge Sort");
 
-
+static void Measure(Func<int[], int[]> action, int[] arr, string name)
+{
+    Stopwatch watch = new Stopwatch();
+    watch.Start();
+    action(arr);
+    watch.Stop();
+    Console.WriteLine($"{name} N={arr.Length} --> {watch.ElapsedMilliseconds}");
+}
 
 
 
--- 071.Quicksort/Program.cs
+++ 071.Quicksort/Program.cs
@@ -1,9 +1,29 @@
 ﻿using _071.Quicksort;
+using System.Diagnostics;
 
-var array = new int[] { 1, 55, 33, 45345, 5445, 23, 5, 65 };
+//var array = new int[] { 1, 55, 33, 45345, 5445, 23, 5, 65 };
 
-Console.WriteLine($"Array: {string.Join(", ", array)}");
+//Console.WriteLine($"Array: {string.Join(", ", array)}");
 
-QuickSorter.QuickSort(array, 0, array.Length - 1);
+//QuickSorter.QuickSort(array, 0, array.Length - 1);
 
-Console.WriteLine($"Sorted array: {string.Join(", ", array)}");
+//Console.WriteLine($"Sorted array: {string.Join(", ", array)}");
+
+int n = 100_000;
+Random random = new Random();
+int[] arr = new int[n];
+for (int i = 0; i < n; i++)
+{
+    arr[i] = random.Next();
+}
+
+Measure(QuickSorter.QuickSort, arr, "Quick Sort", 0, arr.Length - 1);
+
+static void Measure(Action<int[], int, int> action, int[] arr, string name, int start, int end)
+{
+    Stopwatch watch = new Stopwatch();
+    watch.Start();
+    action(arr, start, end);
+    watch.Stop();
+    Console.WriteLine($"{name} N={arr.Length} --> {watch.ElapsedMilliseconds}");
+}

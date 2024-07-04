e541cd165e500f3aa5be539f003016222bac1825
bluesbreak <ivanivanovat@gmail.com>
2024-07-04 19:52:08 +0300

    QuickSort algorithm added

---
+++ 071.Quicksort/Program.cs
@@ -0,0 +1,9 @@
+﻿using _071.Quicksort;
+
+var array = new int[] { 1, 55, 33, 45345, 5445, 23, 5, 65 };
+
+Console.WriteLine($"Array: {string.Join(", ", array)}");
+
+QuickSorter.QuickSort(array, 0, array.Length - 1);
+
+Console.WriteLine($"Sorted array: {string.Join(", ", array)}");
---
+++ 071.Quicksort/QuickSorter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _071.Quicksort
+{
+    public class QuickSorter
+    {
+        public static void QuickSort(int[] array, int start, int end)
+        {
+            if (start >= end || start < 0)
+            {
+                return;
+            }
+
+            int pivotIndex = Partitioner(array, start, end);
+
+            QuickSort(array, start, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, end);
+        }
+
+        private static int Partitioner(int[] array, int start, int end)
+        {
+            int pivot = array[end];
+            int pivotIndex = start - 1;
+            for (int i = start; i <= end -1; i++)
+            {
+                if (array[i] <= pivot)
+                {
+                    pivotIndex++;
+                    Swap(array, i, pivotIndex);
+                }
+            }
+
+            pivotIndex++;
+            Swap(array, pivotIndex, end);
+
+            return pivotIndex;
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
